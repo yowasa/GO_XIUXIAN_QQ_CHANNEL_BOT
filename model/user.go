c9f59fc2ce0cf84672d0013d64d63d73168b8407
chrishuang <chrishuang@tvunetworks.com>
2023-01-03 19:53:48 +0800

    feat: test

--- model/user.go
+++ model/user.go
@@ -7,11 +7,34 @@ type User struct {
 	BaseInfo string
 }
 
+// NewUser :create new user by name and id/*
+func NewUser(name string, userId string) *User {
+	var user User = User{
+		UserName: name,
+		UserId:   userId,
+	}
+	// todo 基本属性值
+	return &user
+}
+
 func (u User) Exist() bool {
 	db.Where("UserId = ?", u.UserId).First(&u)
 	if u.Id == 0 {
 		return false
 	}
 	return true
+}
 
+func (u User) ExistName(name string) bool {
+	db.Where("UserName = ?", name).First(&u)
+	if u.Id == 0 {
+		return false
+	}
+	return true
+}
+
+// todo
+func getLingGen() string {
+
+	return ""
 }
