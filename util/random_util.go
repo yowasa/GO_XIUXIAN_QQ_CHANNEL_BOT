8501c75b8b5bcb31817bd7d09c5264c6902ddb6e
yowasa <2508488843@qq.com>
2023-01-04 21:17:15 +0800

    Changes

--- util/random_util.go
+++ util/random_util.go
@@ -6,37 +6,37 @@ import (
 )
 
 var (
-	random_seed = rand.New(rand.NewSource(time.Now().UnixNano())) // 分配的随机数
+	randomSeed = rand.New(rand.NewSource(time.Now().UnixNano())) // 分配的随机数
 )
 
-// 范围随机 包下不包上
+// RandomRange 范围随机 包下不包上
 func RandomRange(min int, max int) int {
-	return random_seed.Intn(max-min) + min
+	return randomSeed.Intn(max-min) + min
 }
 
-// 随机分城多少份 整数
+// RandomDistribution 随机分城多少份 整数
 func RandomDistribution(total int, num int) []uint {
-	left_money, left_num := total, num
-	money_list := make([]uint, num)
-	for left_money > 0 {
+	leftMoney, leftNum := total, num
+	moneyList := make([]uint, num)
+	for leftMoney > 0 {
 
-		if left_num == 1 {
-			money_list[num-1] = uint(left_money)
+		if leftNum == 1 {
+			moneyList[num-1] = uint(leftMoney)
 			break
 		}
 
-		if left_num == left_money {
-			for i := 0; i < left_num; i++ {
-				money_list[i] = 1
+		if leftNum == leftMoney {
+			for i := 0; i < leftNum; i++ {
+				moneyList[i] = 1
 			}
 			break
 		}
 
-		rMoney := int(2 * float64(left_money) / float64(left_num))
-		rand_m := random_seed.Intn(rMoney)
-		money_list[num-left_num] = uint(rand_m)
-		left_money -= rand_m
-		left_num--
+		rMoney := int(2 * float64(leftMoney) / float64(leftNum))
+		randM := randomSeed.Intn(rMoney)
+		moneyList[num-leftNum] = uint(randM)
+		leftMoney -= randM
+		leftNum--
 	}
-	return money_list
+	return moneyList
 }
---
+++ util/str_util.go
@@ -0,0 +1,18 @@
+package util
+
+import "github.com/tencent-connect/botgo/dto"
+
+// 获取 Embed
+func BuildEmbed(title string, picUrl string, msgList []string) *dto.Embed {
+	var fieldList []*dto.EmbedField
+	for _, msg := range msgList {
+		fieldList = append(fieldList, &dto.EmbedField{Name: msg})
+	}
+	return &dto.Embed{
+		Title: title,
+		Thumbnail: dto.MessageEmbedThumbnail{
+			URL: picUrl,
+		},
+		Fields: fieldList,
+	}
+}
